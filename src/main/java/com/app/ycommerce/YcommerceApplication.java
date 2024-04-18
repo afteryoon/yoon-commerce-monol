db88c4434feb3c16fcbd0abc9122849b06863026
afteryoon <khsso102649@gmail.com>
2024-04-19 01:25:35 +0900

    Initial commit

---
+++ src/main/java/com/app/ycommerce/YcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.app.ycommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/ycommerce/YcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
